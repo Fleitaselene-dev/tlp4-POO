00783f8595ea87de0fc0133511f3cbd0db89bc0a
fleita selene <fleitaselene26@gmail.com>
2025-08-07 13:01:03 -0300

    feat: Implementar clase CuentaBancaria con get y set

---
+++ consigna2.js
@@ -0,0 +1,32 @@
+class CuentaBancaria {
+    #saldoInicial;
+    constructor(titular, saldoInicial){
+    this.titular = titular
+    this.#saldoInicial = saldoInicial
+    };
+  get saldoInicial() {
+    return this.#saldoInicial;
+  }
+  set depositar (monto) {
+    if (monto >= 0) {
+      this.#saldoInicial += monto;
+    }
+  }
+
+  set extraer(monto) {
+    if (monto > this.#saldoInicial) {
+      console.log( "Fondos insuficientes");
+    }
+     this.#saldoInicial -= monto;
+  }
+  get consultarSaldo(){
+    console.log(`4. Saldo Actual: $${this.saldoInicial}`)
+  }
+} 
+
+const Cuenta = new CuentaBancaria("Selene Fleita", 10000)
+console.log(`1. Tu saldo: $${Cuenta.saldoInicial}`)
+console.log(`2. Se ha depositado: $${Cuenta.depositar = 2000}`)
+console.log(`3. Haz extraido: $${Cuenta.extraer = 5000}`)
+Cuenta.consultarSaldo
+
