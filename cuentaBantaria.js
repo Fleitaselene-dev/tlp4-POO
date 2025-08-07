27e56ce4331f00cfb109e1e06425068a9bf62750
fleita selene <fleitaselene26@gmail.com>
2025-08-07 12:23:26 -0300

    feat: Agrega función constructora CuentaBancaria con métodos de depósito, extracción y consulta de saldo

---
+++ cuentaBantaria.js
@@ -0,0 +1,22 @@
+
+ function CuentaBancaria (titular, saldoInicial){
+    this.titular = titular
+    this.saldoInicial = saldoInicial
+ }
+ CuentaBancaria.prototype.depositar = function(monto){
+   this.saldoInicial += monto;
+ };
+ CuentaBancaria.prototype.extraer = function(monto){
+  if(this.saldoInicial >= monto ){
+    this.saldoInicial -= monto
+  }else{
+    console.log("Fondos insuficientes")
+  }
+  };
+  CuentaBancaria.prototype.consultarSaldo = function(){
+ console.log(`Saldo Actual: $${this.saldoInicial}`)
+  }
+const Cuenta = new CuentaBancaria("Lucas Cardenas", 10000)
+Cuenta.depositar(2000)
+Cuenta.extraer(5000)
+Cuenta.consultarSaldo()
